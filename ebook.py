77e309dde3b72cbc5acb7ef6fb461ba09733bb8d
Hider <724598894@qq.com>
2019-04-30 14:55:21 +0800

    小说抓取和下载完成

--- ebook.py
+++ ebook.py
@@ -5,32 +5,36 @@ from bs4 import BeautifulSoup
 class downloader(object):
 
     def __init__(self):
-        self.server = 'http://www.biqukan.com/'
-        self.target ='http://www.biqukan.com/0_790/'
+        # self.server = 'https://www.biduo.cc/biquge/'
+        self.target ='https://www.biduo.cc/biquge/42_42269/'
         self.names = []
         self.urls = []
         self.nums = 0
 
 
     def get_download_url(self):
-        req = requests.get(url = self.target)
-        html = req.text
-        div_bf = BeautifulSoup(html)
-        div = div_bf.find_all('div',class_ = 'listmain')
-        a_bf = BeautifulSoup(str(div[0]))
-        a = a_bf.find_all('a')
-        self.nums = len(a[15:])
-        for each in a[15:]:
-            self.names.append(each.string)
-            self.urls.append(self.server + each.get('href'))
+            req = requests.get(url = self.target)
+            req.encoding = 'GBK'
+            html = req.text
+            div_bf = BeautifulSoup(html,'html.parser')
+            texts = div_bf.find(id = 'list') 
+            a_bf = BeautifulSoup(str(texts),'html.parser')
+            a = a_bf.find_all('a')
+            self.nums = len(a)
+            for each in a:
+                # print(each.string, self.server + each.get('href'))
+                self.names.append(each.string)
+                self.urls.append(self.target + each.get('href'))
+    
 
 
     def get_contents(self,target):
         req = requests.get(url=target)
+        req.encoding = 'GBK'
         html = req.text
-        bf = BeautifulSoup(html)
-        texts = bf.find_all('div',class_ = 'showtxt')
-        texts.text.replace('\xa0'*8,'\n\n')
+        bf = BeautifulSoup(html,'html.parser')
+        texts = bf.find(id = 'content') 
+        texts = texts.text.replace('\xa0'*4,'\n\xa0\xa0')
         return texts
 
 
@@ -47,7 +51,7 @@ if __name__ == "__main__":
     print('开始下载:')
     for i in range(dl.nums):
         dl.writer(dl.names[i],'小说.txt',dl.get_contents(dl.urls[i]))
-        sys.stdout.write(" 已下载:%.3f%%" % float(i/dl.nums*100) +'\r')
+        sys.stdout.write(" 已下载:%.2f%%" % float(i/dl.nums*100) +'\r')
         sys.stdout.flush()
-    print('下载完成')
+    print('下载完成 100.00%')
 
